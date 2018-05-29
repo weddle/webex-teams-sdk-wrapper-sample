10fc35e0fb2637bf6a5f685d47c670f38fcd3fc6
Ryan Weddle <weddle@gmail.com>
2018-05-29 15:38:01 -0700

    Initial commit

---
+++ app/src/main/java/com/ryanweddle/webexteamssdkwrappersample/DevConstants.java
@@ -0,0 +1,12 @@
+
+package com.ryanweddle.webexteamssdkwrappersample;
+
+/**
+ * Created by ryweddle on 3/11/18.
+ */
+
+public class DevConstants {
+
+    public static final String EXAMPLE_JWT = "DEFAULT_JWT_TOKEN";
+    public static final String EXAMPLE_SPARKID = "teamsid@domain.com";
+}
